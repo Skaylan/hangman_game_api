a6d1f9a1b4f14b29ba95c179d166cdf39ab29834
skaylan <lucas_7tv@hotmail.com>
2023-01-03 12:24:22 -0300

    small changes

--- app/controllers/config/config.py
+++ app/controllers/config/config.py
@@ -4,5 +4,5 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
-cors = CORS(app, resources={'/api/*': {'origins': ['http://localhost:3000', 'http://127.0.0.1:3000']}})
+cors = CORS(app, resources={'/api/*': {'origins': ['http://localhost:3000', 'http://127.0.0.1:3000', 'http://hangman-game-api.vercel.app']}})
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
